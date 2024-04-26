c4b272acebd0f69c8b92f019b0f041a562b5a7ef
ikhsanytc <ikhsanm181209@gmail.com>
2024-04-26 19:31:22 +0700

    add home pages

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,32 @@
+import feather from "feather-icons";
+
 const Home = () => {
   return (
     <>
-      <h1>Test</h1>
+      <nav className="bg-white border fixed top-0 w-full p-3 shadow">
+        <h1>List Siswa</h1>
+      </nav>
+      <section className="mt-20 flex flex-col gap-4 px-4">
+        <h1 className="font-bold text-lg">Halo!</h1>
+        <div className="bg-slate-300 w-full p-3 rounded-lg flex justify-between active:bg-slate-400">
+          <div className="flex gap-2">
+            <div
+              dangerouslySetInnerHTML={{ __html: feather.icons.user.toSvg() }}
+            ></div>
+            <p>ikhsan</p>
+          </div>
+          <div className="flex gap-2">
+            <div
+              dangerouslySetInnerHTML={{ __html: feather.icons.edit.toSvg() }}
+              className="cursor-pointer"
+            ></div>
+            <div
+              dangerouslySetInnerHTML={{ __html: feather.icons.delete.toSvg() }}
+              className="cursor-pointer"
+            ></div>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
